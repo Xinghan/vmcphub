976e5ec40b19283473d1df6b8ed50f2e0ea30f67
Xinghan Cui <xinghan.cui@gmail.com>
2026-05-25 15:40:42 +0800

    chore: drop github pages workflow and revert config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
   url: 'https://example.invalid',
   baseUrl: '/',
-  organizationName: 'vmware',
+  organizationName: 'Xinghan',
   projectName: 'vmcphub',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
